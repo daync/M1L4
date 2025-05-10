3d9d2b49e75c34cf4d96b6f0c0b09a1198879430
daync <daiyandam@gmail.com>
2025-05-10 21:47:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,31 @@ async def pasw(ctx):
 async def selamatmalam(ctx):
     await ctx.send("selamat malam juga,love love.")
 
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+@bot.command()
+async def guess(ctx):
+    await ctx.send('Guess a number between 1 and 10.')
+
+    def is_correct(m):
+        return m.author == ctx.author and m.channel == ctx.channel and m.content.isdigit()
+
+    answer = random.randint(1, 10)
+
+    try:
+        guess = await bot.wait_for('message', check=is_correct, timeout=5.0)
+    except asyncio.TimeoutError:
+        return await ctx.send(f'Sorry, you took too long. The answer was {answer}.')
+
+    if int(guess.content) == answer:
+        await ctx.send('You are right!')
+    else:
+        await ctx.send(f'Oops. It is actually {answer}.')
+
+
 bot.run("MTM2ODIwNjMyMjgxMzc2MzYyNw.G_buUa.dWb7nuqjXSWNKspubF2vrVYjj9lfUCteczjEN8")
 
 
